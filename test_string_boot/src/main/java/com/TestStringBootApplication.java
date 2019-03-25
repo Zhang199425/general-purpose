2a7fe1193e5d6160aa8012abc3c4598a21f5e077
zhanghang <zhangh@yiyou.com.cn>
2019-03-25 15:02:08 +0800

    新建测试项目

---
+++ test_string_boot/src/main/java/com/TestStringBootApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStringBootApplication.class, args);
+	}
+
+}
